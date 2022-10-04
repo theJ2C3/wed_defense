f8ef884b4876a40f98891b178187c94f472f13b5
theJ2C3 <48209744+theJ2C3@users.noreply.github.com>
2022-10-05 02:23:59 +0800

    Add files via upload

---
+++ 國防點名/autoclick_window.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+import getopt
+import getpass
+import signal
+import time
+from sys import argv
+from random import randint,choice
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from quickstart import sendGmail,sendGmailWithAttachments
+
+def sig_int(signal, frame, sender, reciever):
+	print('quiting...')
+	sendGmail(sender,reciever,"quiting","")
+	driver.quit()
+	exit()
+
+def setup():
+	helpmsg = '''
+Options:
+  -n  SECONDS		 refresh the website every SECONDS seconds (default: 3)
+  --no-sign-in		 the script will not try to sign in the course'''
+	sec = 100
+	need_sign_in = True
+	try:
+		opts, args = getopt.getopt(argv[1:], 'n:', ['help','no-sign-in'])
+		for opt, arg in opts:
+			if opt == '--help':
+				print(helpmsg)
+				exit()
+			elif opt == '-n':
+				sec = arg
+			elif opt == '--no-sign-in':
+				need_sign_in = False
+	except getopt.GetoptError:
+		print(helpmsg)
+		exit(1)
+	
+	if len(args) > 1:
+		print('Error: too many urls')
+		print(helpmsg)
+		exit(2)
+	elif len(args) < 1:
+
+# ---------------------------------------------------------------------------- #
+#                             Enter Your Course ID                             #
+# ---------------------------------------------------------------------------- #
+
+		# print('Error: missing the url, set course "自動化測試實習" as default')
+		# url = "https://irs.zuvio.com.tw/student5/irs/clickers/504929"
+		print('Error: missing the url, set course "國防政策" as default')
+		# url = "https://irs.zuvio.com.tw/student5/irs/clickers/725547"
+		url = "https://irs.zuvio.com.tw/student5/irs/rollcall/981470"
+		# url = "https://irs.zuvio.com.tw/student5/irs/clickers/981470"
+		print(helpmsg)
+	else:
+		url = args[0]
+
+	return sec, need_sign_in, url
+
+
+sec, need_sign_in, url = setup()
+
+# ---------------------------------------------------------------------------- #
+#                            Enter Your Account Here                           #
+# ---------------------------------------------------------------------------- #
+
+# email = input('Enter your email: ')
+# email = "YOUR ACCOUNT"
+email = "b07207073@ntu.edu.tw"
+# password = getpass.getpass()
+# password = "YOUR PASSWORD"
+password = "123"
+
+# options = webdriver.firefox.options.Options()
+# options.add_argument('--headless') #hide the browser window
+
+# ---------------------------------------------------------------------------- #
+#                              Enter Your Location                             #
+# ---------------------------------------------------------------------------- #
+
+params = {
+    "latitude": 25.018469,
+    "longitude": 121.536851,
+    "accuracy": 100
+}
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome()#chrome_options=chrome_options
+driver.execute_cdp_cmd("Page.setGeolocationOverride", params)
+driver.get('https://irs.zuvio.com.tw')
+
+# signal.signal(signal.SIGINT, sig_int)
+
+login = False
+while not login:
+	# driver.find_element_by_id('email').send_keys(email)
+	driver.find_element("id", "email").send_keys(email)
+	# driver.find_element_by_id('password').send_keys(password)
+	driver.find_element("id", "password").send_keys(password)
+	driver.find_element("id", 'login-btn').click()
+	driver.implicitly_wait(1)
+	try:
+		driver.find_element("id", 'login_btn')
+	except NoSuchElementException: #if it does not exist the login button, which means you have logged in
+		login=True
+	else:
+		print('Wrong email or password. Please try again.')
+		email = input('Enter your email: ')
+		password = getpass.getpass()
+
+del password
+print('Logged in successfully')
+
+# ---------------------------------------------------------------------------- #
+#                           Enter Your Email Address                           #
+# ---------------------------------------------------------------------------- #
+
+# sender = "Your Email Address"
+sender = "jay25262753@gmail.com"
+# reciever = "Your Email Address"
+reciever = "jay25262753@gmail.com"
+# sendGmail(sender,reciever,'Logged in successfully',"")
+# ---------------------------------------send email------------------------
+driver.get(url)
+
+signed_in = not need_sign_in
+sign_in_url=url.replace('clickers','rollcall')
+
+
+recurssive = True
+while recurssive or not signed_in:			
+	if recurssive or not signed_in:
+		driver.get(sign_in_url)
+		time.sleep(sec)
+		try:			
+			driver.save_screenshot("The_shot.png")
+			driver.find_element("id", 'submit-make-rollcall').click()
+			print(str(datetime.now().time())[:8]+' signed in')
+			# time.sleep(5)
+			# driver.save_screenshot("The shot_2.png")
+			# sendGmailWithAttachments(sender,reciever,'Signed in',str(datetime.now().time())[:8]+' signed in',"The_shot.png")
+			# signed_in = True
+
+		except NoSuchElementException:
+			print(str(datetime.now().time())[:8]+"not started yet")
+			# sendGmail(sender,reciever,'NO such element',"")
+			pass
+	try:
+		driver.refresh()
+	except:
+		print('{{{'+str(datetime.now().time())[:8]+'}}} connection timeout')
+		# sendGmail(sender,reciever,"CONNECTION ERROR","")
+		time.sleep(30)
+		continue
---
+++ 國防點名/quickstart.py
@@ -0,0 +1,170 @@
+from __future__ import print_function
+import pickle
+import os.path
+import base64
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+from googleapiclient.errors import HttpError
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://mail.google.com/']
+
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode("utf-8")).decode('ascii')}
+
+def create_message_with_attachment(
+    sender, to, subject, message_text, file):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+    file: The path to the file to be attached.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEMultipart()
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+
+  msg = MIMEText(message_text)
+  message.attach(msg)
+
+  content_type, encoding = mimetypes.guess_type(file)
+
+  if content_type is None or encoding is not None:
+    content_type = 'application/octet-stream'
+  main_type, sub_type = content_type.split('/', 1)
+  if main_type == 'text':
+    fp = open(file, 'rb')
+    msg = MIMEText(fp.read(), _subtype=sub_type)
+    fp.close()
+  elif main_type == 'image':
+    fp = open(file, 'rb')
+    msg = MIMEImage(fp.read(), _subtype=sub_type)
+    fp.close()
+  elif main_type == 'audio':
+    fp = open(file, 'rb')
+    msg = MIMEAudio(fp.read(), _subtype=sub_type)
+    fp.close()
+  else:
+    fp = open(file, 'rb')
+    msg = MIMEBase(main_type, sub_type)
+    msg.set_payload(fp.read())
+    fp.close()
+  filename = os.path.basename(file)
+  msg.add_header('Content-Disposition', 'attachment', filename=filename)
+  message.attach(msg)
+
+  return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+
+    Returns:
+      Sent Message.
+    """
+#   try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print('Message Id: %s' % message['id'])
+    return message
+#   except HttpError:
+#     print("QQQQQ")
+#     print('An error occurred:',HttpError )
+
+
+def authenticate():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = authenticate()
+    service = build('gmail', 'v1', credentials=creds)
+
+    content = '''
+Dear Lin:
+
+test message
+
+Best,
+
+Hung-Chun,Lin'''
+    mes = create_message_with_attachment("penguin7296@gmail.com",
+                         "b06901049@g.ntu.edu.tw", "TEST", content,"The shot.png")
+    send_message(service, "me", mes)
+
+
+def sendGmail(sender, to, subject="", content=""):
+    creds = authenticate()
+    service = build('gmail', 'v1', credentials=creds)
+
+    mes = create_message(sender, to, subject, content)
+    send_message(service, "me", mes)
+
+def sendGmailWithAttachments(sender, to, subject="", content="",filepath=""):
+    creds = authenticate()
+    service = build('gmail', 'v1', credentials=creds)
+
+    mes = create_message_with_attachment(sender, to, subject, content, filepath)
+    send_message(service, "me", mes)
+
+
+
+if __name__ == '__main__':
+    main()
